cbd699af56b7a4c236e2ca67ca5a556d50c4f4f2
Andres Uris <andres.urismontilla@gmail.com>
2023-01-21 13:30:00 +0100

    refactor(lib.rs): make run to return a server running without waiting

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,17 @@
 use actix_web::{ web, App, HttpResponse, HttpServer };
+use actix_web::dev::Server;
 
 async fn ping() -> HttpResponse {
   HttpResponse::Ok().finish()
 }
 
-pub async fn run() -> std::io::Result<()> {
-  HttpServer::new(|| {
+pub fn run() -> Result<Server, std::io::Error> {
+  let server = HttpServer::new(|| {
     App::new()
       .route("/ping", web::get().to(ping))
   })
   .bind("127.0.0.1:8000")?
-  .run()
-  .await
+  .run();
+
+  Ok(server)
 }
--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,5 @@ use newsletter_with_rust::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-  run().await
+  run()?.await
 }
