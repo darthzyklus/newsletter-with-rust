a4d4c04b77b14d40053a857ec21c41ae1adcb90d
Andres Uris <andres.urismontilla@gmail.com>
2023-01-21 20:14:20 +0100

    style: apply format with fmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,16 @@
 use std::net::TcpListener;
 
-use actix_web::{ web, App, HttpResponse, HttpServer };
 use actix_web::dev::Server;
+use actix_web::{web, App, HttpResponse, HttpServer};
 
 async fn ping() -> HttpResponse {
-  HttpResponse::Ok().finish()
+    HttpResponse::Ok().finish()
 }
 
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-  let server = HttpServer::new(|| {
-    App::new()
-      .route("/ping", web::get().to(ping))
-  })
-  .listen(listener)?
-  .run();
+    let server = HttpServer::new(|| App::new().route("/ping", web::get().to(ping)))
+        .listen(listener)?
+        .run();
 
-  Ok(server)
+    Ok(server)
 }
--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,6 @@ use newsletter_with_rust::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-  let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind random port");
-  run(listener)?.await
+    let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind random port");
+    run(listener)?.await
 }
