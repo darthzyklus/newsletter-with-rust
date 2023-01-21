e776d4e5d0d0cfe2bb233a63497e4966a292c23a
Andres Uris <andres.urismontilla@gmail.com>
2023-01-21 12:39:20 +0100

    feat: implement ping endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{ web, App, HttpRequest, HttpResponse, HttpServer, Responder };
+
+async fn ping() -> impl Responder {
+  HttpResponse::Ok().finish()
+}
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+  HttpServer::new(|| {
+    App::new()
+      .route("/ping", web::get().to(ping))
+  })
+  .bind("127.0.0.1:8000")?
+  .run()
+  .await
 }
